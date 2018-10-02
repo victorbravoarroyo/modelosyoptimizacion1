c31500fd1ee9905eb457ea7eb1a9923abaca2fb6
Victor Bravo <alqace@gmail.com>
2018-10-02 08:50:25 -0300

    una posible solucion

--- Primer Entrega/ParteB.mod
+++ Primer Entrega/ParteB.mod
@@ -1,18 +1,32 @@
-var VIP >= 0, integer;
-var GRAL >= 0, integer;
-var PAQ >= 0, integer;
-var PAQVIP >= 0, integer;
-var PAQGRAL >= 0, integer;
+# V: Cantidad de ubicaciones VIP en unidad/m^2
+# Vc: Cantidad de ubicaciones VIP comunes en unidad/m^2
+# Vp: Cantidad de ubicaciones VIP de protocolo en unidad/m^2
+# G: Cantidad de ubicaciones generales en unidad/m^2
+# Evc: Cantidad de entradas VIP comun en unidad.
+# Evp: Cantidad de entradas VIP de protocolo en unidad.
+# Eg: Cantidad de entradas generales en unidad.
+# P: Cantidad de paquetes en unidad. 
 
-maximize z: 1500*(VIP - 100) + 800*GRAL - 700*PAQ;
+var V >= 0, integer;
+var Vc >= 0, integer;
+var Vp >= 0, integer;
+var G >= 0, integer;
+var Evc >= 0, integer;
+var Evp >= 0, integer;
+var Eg >= 0, integer;
+var P >= 0, integer;
 
-s.t. procEq1: 1*VIP + 0.5*GRAL <= 8000;
-s.t. procEq2: VIP >= 100;
-s.t. procEq3: GRAL >= 500;
-s.t. procEq4: PAQ - PAQVIP - PAQGRAL = 0;
-s.t. procEq5: (1/8)*GRAL - PAQGRAL = 0;
-s.t. procEq6: (1/20)*VIP - PAQVIP = 0;
-s.t. procEq7: PAQ <= 800;
+maximize z: 1000 * Evc + 800 * Eg + 0 * Evp - 700 * P;
 
+s.t. predio: V + G <= 8000;
+s.t. ubicacionesVip: V = Vc + Vp;
+s.t. protocolo: Vp >= 100;
+s.t. generales: G >= 500;
+s.t. paquetes: P <= 800;
+s.t. paqueteVip: 20*P = V;
+s.t. paqueteGeneral: 8*P = Eg;
+s.t. ubicEntrVipProto: Vp = Evp;
+s.t. ubicEntrVipComun: Vc = Evc;
+s.t. ubicGeneral: 2*G = Eg; 
 
 end;
--- Primer Entrega/ParteB.sol
+++ Primer Entrega/ParteB.sol
@@ -1,28 +1,38 @@
 Problem:    ParteB
-Rows:       8
-Columns:    5 (5 integer, 0 binary)
-Non-zeros:  15
+Rows:       11
+Columns:    8 (8 integer, 0 binary)
+Non-zeros:  21
 Status:     INTEGER OPTIMAL
-Objective:  z = 11559600 (MAXimum)
+Objective:  z = 8458100 (MAXimum)
 
    No.   Row name        Activity     Lower bound   Upper bound
 ------ ------------    ------------- ------------- -------------
-     1 z                 1.17096e+07                             
-     2 procEq1                  8000                        8000 
-     3 procEq2                  7740           100               
-     4 procEq3                   520           500               
-     5 procEq4                     0            -0             = 
-     6 procEq5                     0            -0             = 
-     7 procEq6                     0            -0             = 
-     8 procEq7                   452                         800 
+     1 z                  8.4581e+06                             
+     2 predio                   7992                        8000 
+     3 ubicacionesVip
+                                   0            -0             = 
+     4 protocolo                 100           100               
+     5 generales                1332           500               
+     6 paquetes                  333                         800 
+     7 paqueteVip                  0            -0             = 
+     8 paqueteGeneral
+                                   0            -0             = 
+     9 ubicEntrVipProto
+                                   0            -0             = 
+    10 ubicEntrVipComun
+                                   0            -0             = 
+    11 ubicGeneral                 0            -0             = 
 
    No. Column name       Activity     Lower bound   Upper bound
 ------ ------------    ------------- ------------- -------------
-     1 VIP          *           7740             0               
-     2 GRAL         *            520             0               
-     3 PAQ          *            452             0               
-     4 PAQVIP       *            387             0               
-     5 PAQGRAL      *             65             0               
+     1 V            *           6660             0               
+     2 Vc           *           6560             0               
+     3 Vp           *            100             0               
+     4 G            *           1332             0               
+     5 Evc          *           6560             0               
+     6 Evp          *            100             0               
+     7 Eg           *           2664             0               
+     8 P            *            333             0               
 
 Integer feasibility conditions:
 
