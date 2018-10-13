693dd7cc5c56b07963e19b653fbfe5a0d11da32c
Maxi Romero <maxi_96_14@hotmail.com>
2018-10-13 11:24:46 -0300

    Cambios en seleccion

--- Primer Entrega/seleccionParteA.sol
+++ Primer Entrega/seleccionParteA.sol
@@ -1,23 +1,64 @@
-  No.   Row name        Activity     Lower bound   Upper bound
+ No.   Row name        Activity     Lower bound   Upper bound
 ------ ------------    ------------- ------------- -------------
      1 z                          97        
   4220 trioDeSimbolos              4                           4  
-    52 Ype[9,4,NOR] *              1             0             1
-    56 Ype[10,2,NOR]*              1             0             1  
-    75 Ype[13,3,NOR]*              1             0             1 
-    91 Ype[16,1,NOR]*              1             0             1  
-   102 Ype[17,6,NOR]*              1             0             1
-   107 Ype[18,5,NOR]*              1             0             1   
-   335 Yse[8,5,NOR] *              1             0             1 
-   354 Yse[11,6,NOR]*              1             0             1 
-   355 Yse[12,1,NOR]*              1             0             1
-   364 Yse[13,4,NOR]*              1             0             1   
-   369 Yse[14,3,NOR]*              1             0             1 
-   374 Yse[15,2,NOR]*              1             0             1
-   574 Yte[6,4,NOR] *              1             0             1
-   579 Yte[7,3,NOR] *              1             0             1 
-   584 Yte[8,2,NOR] *              1             0             1 
-   593 Yte[9,5,NOR] *              1             0             1
-   600 Yte[10,6,NOR]*              1             0             1
-   601 Yte[11,1,NOR]*              1             0             1      
-                  
+    52 Ype[9,4,NOR]               1             0             1
+    56 Ype[10,2,NOR]              1             0             1  
+    75 Ype[13,3,NOR]              1             0             1 
+    91 Ype[16,1,NOR]              1             0             1  
+   102 Ype[17,6,NOR]              1             0             1
+   107 Ype[18,5,NOR]              1             0             1   
+   335 Yse[8,5,NOR]               1             0             1 
+   354 Yse[11,6,NOR]              1             0             1 
+   355 Yse[12,1,NOR]              1             0             1
+   364 Yse[13,4,NOR]              1             0             1   
+   369 Yse[14,3,NOR]              1             0             1 
+   374 Yse[15,2,NOR]              1             0             1
+   574 Yte[6,4,NOR]               1             0             1
+   579 Yte[7,3,NOR]               1             0             1 
+   584 Yte[8,2,NOR]               1             0             1 
+   593 Yte[9,5,NOR]               1             0             1
+   600 Yte[10,6,NOR]              1             0             1
+   601 Yte[11,1,NOR]              1             0             1 
+   745 monedasDisponibles[1,1]
+                   *              3             0               
+   746 monedasDisponibles[1,2]
+                   *              1             0               
+   747 monedasDisponibles[1,3]
+                   *              1             0               
+   748 monedasDisponibles[1,4]
+                   *              1             0               
+   749 monedasDisponibles[1,5]
+                   *              1             0               
+   750 monedasDisponibles[1,6]
+                   *              1             0               
+   751 monedasDisponibles[2,1]
+                   *              1             0               
+   752 monedasDisponibles[2,2]
+                   *              1             0               
+   753 monedasDisponibles[2,3]
+                   *              1             0               
+   754 monedasDisponibles[2,4]
+                   *              1             0               
+   755 monedasDisponibles[2,5]
+                   *              0             0
+   763 gastoMonedas[1,1]
+                   *              2             0
+   772 gastoMonedas[2,4]
+                   *              1             0 
+ 
+   908 recursosComprados[1,1,TEL]
+                   *              1             0   
+   975 recursosComprados[2,4,MAD]
+                   *              1             0  
+
+  1035 Geometricas *              4             0               
+  1039 Yg4         *              1             0             1 
+  
+  1040 Ruedas      *              4             0               
+  1044 Yr4         *              1             0             1
+
+  1045 Escrituras  *              5             0               
+  1050 Ye5         *              1             0             1 
+  
+  1052 desarrollo  *              0             0    
