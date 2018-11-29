b0154366b2e52fccfa4e1c4cd73b65b381e67909
Maxi Romero <maxi9614@gmail.com>
2018-11-29 02:46:46 -0300

    Heuristica con 86ptos

--- Tercer Entrega/Mazo.py
+++ Tercer Entrega/Mazo.py
@@ -1,54 +1,44 @@
-#Era III:
-#puede hacerse las 6 de forma gratuita
-#    (usa LibraryX2, Laboratory, SchoolX2, Dispensary)
+def setearRecursosNecesarios(recNeces):
+    for recurso in recNeces:
+        recNeces[recurso] = 0
 
-#Era II:
-#hay 3/4 que pueden ser gratis[Disp,Lab,Lib]
-#    (usa Apothecary, Workshop, Scriptorium)
-#    SCHOOL: 1 madera y 1 papiro
-
-#Era I:
-#    APOTHECARY: 1 Tela
-#    WORKSHOP: 1 Ceramica
-#    SCRIPTORIUM: 1 Papiro
-
-#Definitiva: necesitaria 1 Tela, 1 Madera(E2), 1 Ceramica y 1 Papiro
-#    Podria comprar solo 1 recurso.
-#    Era 1: Classwork(1Cer) - Press (1 Pap) - Loom (1Tela). Todas Gratis
-
-#    Era II: Sawmill (2 Maderas).
-
-#Nombre, Tipo, Costos..., Gratis, Especialidad...
-    #Costos: Monedas, Lad, Cem, Oro, Mad, Cer, Pap, Tel.
-    #Especi: Monedas, Lad, Cem, Oro, Mad, Cer, Pap, Tel, Mil, Ptos.    
 class Carta:
     def __init__(self, nombre, Tipo, CostosTot, CMon, CLad, CCem, COro, CMad, CCer, CPap, CTel, Gratis, EMon, ELad, ECem, EOro, EMad, ECer, EPap, ETel, EGeo, ERue, EEsc, EMil, EPto):
         self.Nombre = nombre
         self.Tipo = Tipo
         self.CTot = CostosTot
-        self.CMon = CMon
-        self.CLad = CLad
-        self.CCem = CCem
-        self.COro = COro
-        self.CMad = CMad
-        self.CCer = CCer
-        self.CPap = CPap
-        self.CTel = CTel
+        self.Costos = {'Mon': CMon, 'Lad': CLad, 'Cem': CCem,
+                       'Oro': COro, 'Mad': CMad, 'Cer': CCer,
+                       'Pap': CPap, 'Tel': CTel}
         self.Gratis = Gratis
-        self.EMon = CMon
-        self.ELad = CLad
-        self.ECem = CCem
-        self.EOro = COro
-        self.EMad = CMad
-        self.ECer = CCer
-        self.EPap = CPap
-        self.ETel = CTel
-        self.EGeo = EGeo
-        self.ERue = ERue
-        self.EEsc = EEsc
-        self.EMil = EMil
-        self.EPto = EPto
+        self.Especialidad = {'Mon': EMon, 'Lad': ELad, 'Cem': ECem, 'Oro': EOro,
+                             'Mad': EMad, 'Cer': ECer, 'Pap': EPap, 'Tel': ETel,
+                             'Geo': EGeo, 'Rue': ERue, 'Esc': EEsc, 'Mil': EMil,
+                             'Pto': EPto}
 
+    def sePuedeJugar(self, recDisponibles, cartasAnteriores):
+        if self.esGratis(cartasAnteriores):
+            return True
+        
+        for i in recDisponibles:
+            if (self.Costos[i] > recDisponibles[i]):
+                return False
+            
+        return True
+
+    def esGratis(self, cartasAnteriores):
+        for unaCarta in cartasAnteriores:
+            if (self.Gratis == unaCarta.Nombre):
+                return True
+
+        if (self.CTot == 0):
+            return True
+        return False
+
+    def obetnerRecursosNecesarios(self, recNeces):
+        for i in recNeces:
+            recNeces[i] += self.Costos[i]
+        
     def __lt__(self, other):
         if self.Tipo < other.Tipo:
             return True
@@ -57,6 +47,21 @@ class Carta:
         else:
             return self.ordenarSegunGratis(other)
 
+    def entregaNecesario(self, recNeces):
+        for recurso in recNeces:
+            if (recNeces[recurso] != 0):
+                if (self.Especialidad[recurso] >= recNeces[recurso]):
+                    return True
+        return False
+
+    def realizarEspecilidad(self, recNeces, recDisponibles):
+        for recurso in recNeces:
+            recDisponibles[recurso] += self.Especialidad[recurso]
+            recNeces[recurso] -= self.Especialidad[recurso]
+            if recNeces[recurso] < 0:
+                recNeces[recurso] = 0
+        recDisponibles['Mon'] -= self.Costos['Mon']    
+            
     def ordenarSegunGratis(self, other):
         if (self.Gratis != 0) and (other.Gratis == 0):
             return True
@@ -80,19 +85,19 @@ class Carta:
             return False  
       
 #----- Materias Primas Era 1 -------#
-Clay = Carta('ClayPool', 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
-Stone = Carta('StonePit', 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
-ForCa = Carta('ForestCave', 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0)
-TimYa = Carta('TimberYard', 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0)
-OreVe = Carta('OreVein', 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0)
-Excav = Carta('Excavation', 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
-ClPit = Carta('ClayPit', 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
-LumYa = Carta('LumberYard', 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0)
+Clay = Carta('ClayPool', 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+Stone = Carta('StonePit', 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+ForCa = Carta('ForestCave', 2, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+TimYa = Carta('TimberYard', 2, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0)
+OreVe = Carta('OreVein', 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+Excav = Carta('Excavation', 2, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+ClPit = Carta('ClayPit', 2, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+LumYa = Carta('LumberYard', 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0)
 
 #----- Manufacturas Era 1 -------#
-ClasW = Carta('Classworks', 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0)
-Press = Carta('Press', 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0)
-Loom = Carta('Loom', 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0)
+ClasW = Carta('Classworks', 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0)
+Press = Carta('Press', 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0)
+Loom = Carta('Loom', 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0)
 
 #----- Civilies Era 1 ------#
 Altar = Carta('Altar', 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2)
@@ -190,7 +195,7 @@ Univ = Carta('University', 0, 4, 0, 0, 0, 0, 2, 1, 1, 0, 'Library', 0, 0, 0, 0, 
 Obse = Carta('Observatory', 0, 4, 0, 0, 0, 2, 0, 1, 0, 1, 'Laboratory', 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0)
 Study = Carta('Study', 0, 3, 0, 0, 0, 0, 1, 0, 1, 1, 'School', 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0)
 Acad = Carta('Academy', 0, 4, 0, 0, 3, 0, 0, 1, 0, 0, 'School', 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0)
-Lod = Carta('Lodege', 0, 4, 0, 2, 0, 0, 0, 0, 1, 1, 'Dispensary', 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0)
+Lod = Carta('Lodge', 0, 4, 0, 2, 0, 0, 0, 0, 1, 1, 'Dispensary', 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0)
 Univ2 = Carta('University', 0, 4, 0, 0, 0, 0, 2, 1, 1, 0, 'Library', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0)
 
 #----- Militares Era 3 ------#
@@ -208,11 +213,163 @@ Light = Carta('Lighthouse', 5, 2, 0, 0, 1, 0, 0, 1, 0, 0, 'Caravansery', 0, 0, 0
 mazoEra3 = [Palace, Townha, Senate, Panth, Gardens, Univ, Obse, Study, Acad, Lod, Univ2, Arse, Circus, SiegW, Fort,
             Haven, COC, Light]
 
-for i in mazoEra3:
+cartasJugadasEra1 = []
+cartasJugadasEra2 = []
+cartasJugadasEra3 = []
+
+
+recursos = {'Mon' : 3, 'Lad': 0, 'Cem': 0, 'Oro': 0, 'Mad': 0, 'Cer': 0,
+            'Pap': 0, 'Tel': 0}
+
+recNeces = {'Lad': 0, 'Cem': 0, 'Oro': 0, 'Mad': 0, 'Cer': 0,
+            'Pap': 0, 'Tel': 0}
+
+"""ClasW, Press, Loom"""
+for i in cartasJugadasEra1:
+    i.realizarEspecilidad(recNeces, recursos)
+
+mazoEra1 = sorted(mazoEra1)
+        
+while len(cartasJugadasEra1) < 6:
+    setearRecursosNecesarios(recNeces)
+    for unaCarta in mazoEra1:
+        if (unaCarta.Tipo == 0):
+            if unaCarta.sePuedeJugar(recursos, cartasJugadasEra2):
+                cartaJugada = unaCarta
+                mazoEra1.remove(unaCarta)
+                cartasJugadasEra1.append(cartaJugada)
+                break
+            else:
+                unaCarta.obetnerRecursosNecesarios(recNeces)
+            #break       
+        elif (unaCarta.Tipo == 1) or (unaCarta.Tipo == 2):
+            if unaCarta.entregaNecesario(recNeces):
+                if unaCarta.sePuedeJugar(recursos, cartasJugadasEra2):
+                    cartaJugada = unaCarta
+                    mazoEra1.remove(unaCarta)
+                    cartasJugadasEra1.append(cartaJugada)
+                    cartaJugada.realizarEspecilidad(recNeces, recursos)
+                    break
+
+print ('---- ERA 1 -------')
+for i in cartasJugadasEra1:
     print (i.Nombre)
-print('--------------')
+print ('---- FIN ERA 1 -------')
+
+mazoEra2 = sorted(mazoEra2)
+while len(cartasJugadasEra2) < 6:
+    setearRecursosNecesarios(recNeces)
+    for unaCarta in mazoEra2:
+        if (unaCarta.Tipo == 0):
+            if unaCarta.sePuedeJugar(recursos, cartasJugadasEra1):
+                cartaJugada = unaCarta
+                mazoEra2.remove(unaCarta)
+                cartasJugadasEra2.append(cartaJugada)
+                break
+            else:
+                unaCarta.obetnerRecursosNecesarios(recNeces)
+            #break       
+        elif (unaCarta.Tipo == 1) or (unaCarta.Tipo == 2):
+            if unaCarta.entregaNecesario(recNeces):
+                if unaCarta.sePuedeJugar(recursos, cartasJugadasEra1):
+                    cartaJugada = unaCarta
+                    mazoEra2.remove(unaCarta)
+                    cartasJugadasEra2.append(cartaJugada)
+                    cartaJugada.realizarEspecilidad(recNeces, recursos)
+                    break
+        else:
+            if unaCarta.sePuedeJugar(recursos, cartasJugadasEra1):
+                cartaJugada = unaCarta
+                mazoEra2.remove(unaCarta)
+                cartasJugadasEra2.append(cartaJugada)
+                cartaJugada.realizarEspecilidad(recNeces, recursos)
+                break
+
+print ('---- ERA 2 -------')
+for i in cartasJugadasEra2:
+    print (i.Nombre)
+print ('---- FIN ERA 2 -------')
+
+
 mazoEra3 = sorted(mazoEra3)
-for i in mazoEra3:
+while len(cartasJugadasEra3) < 6:
+    setearRecursosNecesarios(recNeces)
+    for unaCarta in mazoEra3:
+        if (unaCarta.Tipo == 0):
+            if unaCarta.sePuedeJugar(recursos, cartasJugadasEra2):
+                cartaJugada = unaCarta
+                mazoEra3.remove(unaCarta)
+                cartasJugadasEra3.append(cartaJugada)
+                break
+            else:
+                unaCarta.obetnerRecursosNecesarios(recNeces)
+            #break       
+        elif (unaCarta.Tipo == 1) or (unaCarta.Tipo == 2):
+            if unaCarta.entregaNecesario(recNeces):
+                if unaCarta.sePuedeJugar(recursos, cartasJugadasEra2):
+                    cartaJugada = unaCarta
+                    mazoEra3.remove(unaCarta)
+                    cartasJugadasEra3.append(cartaJugada)
+                    cartaJugada.realizarEspecilidad(recNeces, recursos)
+                    break
+        else:
+            if unaCarta.sePuedeJugar(recursos, cartasJugadasEra2):
+                cartaJugada = unaCarta
+                mazoEra3.remove(unaCarta)
+                cartasJugadasEra3.append(cartaJugada)
+                cartaJugada.realizarEspecilidad(recNeces, recursos)
+                break
+
+print ('---- ERA 3 -------')
+for i in cartasJugadasEra3:
     print (i.Nombre)
-print('--------------')
-print(len(mazoEra3))
+print ('---- FIN ERA 3 -------')
+
+
+simbolos = {'Geo': 0, 'Rue': 0, 'Esc': 0}
+
+for unSimbolo in simbolos:
+    for unaCarta in cartasJugadasEra1:
+        simbolos[unSimbolo] += unaCarta.Especialidad[unSimbolo]
+    for unaCarta in cartasJugadasEra2:
+        simbolos[unSimbolo] += unaCarta.Especialidad[unSimbolo]
+    for unaCarta in cartasJugadasEra3:
+        simbolos[unSimbolo] += unaCarta.Especialidad[unSimbolo]        
+
+for unSimbolo in simbolos:
+    print (unSimbolo, simbolos[unSimbolo], sep = ' = ')
+
+puntosCartas = 0
+for unaCarta in cartasJugadasEra1:
+    puntosCartas += unaCarta.Especialidad['Pto']
+for unaCarta in cartasJugadasEra2:
+    puntosCartas += unaCarta.Especialidad['Pto']
+for unaCarta in cartasJugadasEra3:
+    puntosCartas += unaCarta.Especialidad['Pto']    
+
+print ('Puntos por cartas ', puntosCartas, sep = ' = ')
+
+puntosPorMonedas = recursos['Mon'] // 3
+print ('Puntos por monedas', puntosPorMonedas, sep = ' = ')
+
+
+puntosTotales = 0
+for unSim in simbolos:
+    puntosTotales +=  simbolos[unSim]**2
+
+puntosTrioDeCartas = 7*min(simbolos['Geo'], simbolos['Rue'], simbolos['Esc']) 
+print ('Puntos trio simbolos', puntosTrioDeCartas, sep = ' = ')
+
+puntosTotales += puntosCartas + puntosTrioDeCartas
+
+print ('PUNTOS TOTALES', puntosTotales, sep = ' = ')
+
+
+
+
+
+
+
+
+
+
